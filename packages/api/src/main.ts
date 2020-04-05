1848c143085102978d7792a12af457359894e199
Matthieu Rémy <remy.matthieu@gmail.com>
2020-04-05 16:21:08 +0200

    feat: swagger support.

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const userApiOptions = new DocumentBuilder()
+      .setTitle('Users API')
+      .setDescription('Api to manage users.')
+      .setVersion('1.0.0')
+      .addTag('users')
+      .build();
+
+  const userApiDocument = SwaggerModule.createDocument(app, userApiOptions, );
+  SwaggerModule.setup(`/`, app, userApiDocument);
+
   await app.listen(3000);
 }
 bootstrap();
